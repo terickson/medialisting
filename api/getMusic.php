a3a064c1de63bb78e9cbfdf6f62a2289a0c8ef47
terickson <todde1981@gmail.com>
2012-12-26 18:24:37 -0600

    adding in a fix for words beginning with a .

--- api/getMusic.php
+++ api/getMusic.php
@@ -9,17 +9,17 @@ $returnData = array("sEcho"=>$echo,"aaData"=>array());
 
 foreach($artists as $artist)
 {
-	if((strlen($artist) > 2) && $artist != 'lost+found')
+	if((strlen($artist) > 2) && $artist != 'lost+found' && strrpos ($artist,'.') !== '0')
 	{
 		$albums = scandir($dir.$artist.'/');
 		foreach($albums as $album)
 		{
-			if((strlen($album) > 2) && $album != 'lost+found')
+			if((strlen($album) > 2) && $album != 'lost+found' && strrpos (album,'.') !== '0')
 			{
 				$songs = scandir($dir.$artist.'/'.$album.'/');
 				foreach($songs as $song)
 				{
-					if((strlen($song) > 2) && $song != 'lost+found')
+					if((strlen($song) > 2) && $song != 'lost+found' && strrpos ($song,'.') !== '0')
 					{
 						$musicCounter++;
 						array_push($returnData["aaData"],array($artist, $album, $song));
